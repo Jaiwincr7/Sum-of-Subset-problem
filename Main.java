4756e1ad8972ad019e6c9a41b113e5aa769c5dbc
Jaiwin Chaudhari <104194298+Jaiwincr7@users.noreply.github.com>
2022-12-12 23:42:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,37 @@
 import java.util.*;
 class Main {
-    static void maxSum(int a[], int t)
+     static boolean setsum(int a[], int n, int sum)
     {
-        int min = -99999;
-        int sum = 0, start = 0, end = 0, s = 0;
-
-        for (int i = 0; i < t; i++) {
-            sum += a[i];
-            if (min < sum) {
-                min = sum;
-                start = s; // to get the starting of the array for the maximum subarray
-                end = i; // ending value of the array of the subarray
-            }
- 
-            if (sum < 0) {
-                sum = 0;
-                s = i + 1; // updating Starting and ending according to the maximum sum
-            }
+        if (sum == 0) {
+            return true;
         }
-        System.out.println("Subset with maximum sum is:-");
-        for(int i=start;i<=end;i++){
-            System.out.print(a[i]+" ");
+        if (n < 0 || sum < 0) {
+            return false;
         }
-        System.out.println();
-        System.out.println("Maximum contiguous sum is "+ min);
+        boolean in,out;
+        in = setsum(a, n - 1, sum - a[n]);
+        out = setsum(a, n - 1, sum);
+        return in || out;
     }
  
     public static void main(String[] args)
     {
         Scanner n= new Scanner(System.in);
-        System.out.println("Enter the size of the array :-");
+        System.out.println("Enter the size of the list :-");
         int t=n.nextInt();
+        System.out.println("Enter the value for the list");
         int a[] = new int[t];
         for(int i=0;i<t;i++){
             a[i]=n.nextInt();
         }
-        maxSum(a, t);
+        System.out.println("Enter the sum to be displayed:-");
+        int s=n.nextInt();
+        boolean value=setsum(a, t-1, s);
+       if (value) {
+            System.out.print("Sum exists in the list");
+        }
+        else {
+            System.out.print("Sum does not exists in the list");
+        }
     }
 }
